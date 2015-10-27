32dcd66238ab621f093d7a600cbbf75d40876d77
Robert K. Carroll <robert.carroll@gmail.com>
2015-10-27 15:03:08 -0400

    changed message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 var hostname = os.hostname();
 
 app.get('/', function (req, res) {
-	res.send('<html><body>Welcome back, from Node.js container '+ hostname +'</body></html>');
+	res.send('<html><body>Hello from Node.js container '+ hostname +'</body></html>');
 });
 
 app.listen(8080);
